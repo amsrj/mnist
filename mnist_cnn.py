5901a23b09bc632055cd8d461a366525c0b7c09f
amsrj <knockinonhvns@gmail.com>
2025-04-07 17:18:48 +0900

    Added by ESKim

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -4,6 +4,7 @@ Gets to 99.25% test accuracy after 12 epochs
 16 seconds per epoch on a GRID K520 GPU.
 '''
 # Modified on 2025. 04. 07 by ESKim
+
 from __future__ import print_function
 import keras
 import tensorflow.keras
@@ -17,6 +18,7 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import numpy as np
 import os
+import torch, torchvision # Added by ESKim on 25/04/07
 
 batch_size = 128
 num_classes = 10
